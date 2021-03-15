c97845e1cba577d8ecdd37da8784899a4cb662cf
JL <36197836+j-pus-plus@users.noreply.github.com>
2021-03-15 19:21:40 +0530

    Update Calculator.java
    Logs creation functionality added

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,22 +1,49 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class Calculator {
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
 
-   public  int factorial(int n){
-        if(n==1 | n==0)return 1;
-
-        return n*factorial(n-1);
+    public Calculator() {
     }
 
-   public  double square_root(double n){
-        return Math.sqrt(n);
+    public  int factorial(int n){
+        logger.info("[FACTORIAL] - " + n);
+        int ans=1;
+        for(int i=1;i<=n;i++)
+            ans*=i;
+
+
+        logger.info("[RESULT - FACTORIAL] - " + ans);
+
+        return ans;
+    }
+
+   public  double square_root(double n)
+   {    logger.info("[SQUARE_ROOT] - " + n);
+        double ans= Math.sqrt(n);
+       logger.info("[RESULT - SQUARE_ROOT] - " + ans);
+
+       return ans;
+
     }
 
     public double Natural_logarithm(double n){
-       return Math.log(n);
+        logger.info("[NATURAL_LOGARITHM] - " + n);
+        double ans= Math.log(n);
+        logger.info("[RESULT - NATURAL_LOGARITHM] - " + ans);
+
+        return ans;
+
     }
     public double power(double a, double b){
-       return Math.pow(a,b);
+
+        logger.info("[POWER] - " + a + ", " + b);
+        double ans= Math.pow(a,b);
+        logger.info("[RESULT - POWER] - " + ans);
+
+        return ans;
     }
     public static void main(String[] args) {
        Calculator cal=new Calculator();
@@ -52,6 +79,7 @@ public class Calculator {
                         }
 
                     } catch (InputMismatchException error) {
+                        logger.error("Invalid input, Entered input is not a number");
                         return;
                     }
                     System.out.println("Square of " +number1+ " is : " + cal.square_root(number1));
@@ -85,6 +113,7 @@ public class Calculator {
 
 
                     } catch (InputMismatchException error) {
+                        logger.error("Invalid input, Entered input is not a number");
                         return;
                     }
                     System.out.println("Factorial of " +number+ " is : " + cal.factorial(number));
@@ -117,6 +146,7 @@ public class Calculator {
                         }
 
                     } catch (InputMismatchException error) {
+                        logger.error("Invalid input, Entered input is not a number");
                         return;
                     }
                     System.out.println("Natural logarithm of " +number1+ " is : " + cal.Natural_logarithm(number1));
@@ -137,6 +167,7 @@ public class Calculator {
 
 
                     } catch (InputMismatchException error) {
+                        logger.error("Invalid input, Entered input is not a number");
                         return;
                     }
                     System.out.println(number1 +" power "+ number2 + " is : " + cal.power(number1,number2));
@@ -147,7 +178,10 @@ public class Calculator {
 
                     break;
                 default:
-                    //System.out.println("Exiting....");
+                    System.out.println("Exiting....");
+                    System.out.println();
+                    System.out.println();
+                    System.out.println();
                     return;
             }
         } while (true);
